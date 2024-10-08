8cf85a45b4cb75f76c9c8a9878e4718b572c6611
Salomão de Moraes <130030262+sal0minh0@users.noreply.github.com>
2024-10-08 14:57:22 +0000

    função Sim ou Não ao invés de True e False

--- Listas/lista_encadeada_simples.py
+++ Listas/lista_encadeada_simples.py
@@ -46,7 +46,7 @@ class ListaEncadeadaSimples:
             anterior.prox = atual.prox  # Remove o nó ligando o anterior ao próximo
 
     def buscar(self, valor):
-        """Busca um nó que contenha o valor especificado"""
+        """Busca um nó que contenha o valor que queremos"""
         atual = self.cabeca
         while atual is not None:  # Vai alcaçar até o final da lista
             if atual.valor == valor: # Verificar se o no atual tem um valor que procuramos
@@ -73,13 +73,18 @@ class ListaEncadeadaSimples:
             elementos.append(atual.valor)
             atual = atual.prox
         print(" - ".join(map(str, elementos)))
+        
+    def trocar_true_false_sim_nao(valor):
+        return "Sim" if valor else "Não"
+    resposta = True
+    resposta = False
 
 # Exemplo de uso das funções
 
 # Objeto da Classe
 l = ListaEncadeadaSimples()
 
-#Inserir
+# Inserir
 l.inserir_no_fim(1)
 l.inserir_no_fim(2)
 l.inserir_no_fim(3)
@@ -92,12 +97,17 @@ l.remover(2)
 l.imprimir()  # Saída: 0 -> 1 -> 3
 print("")
 
-# Atualizar
-l.atualizar_lista(3, 3) 
+#Atualizar
+l.atualizar_lista(1, 5) # Sabendo o elemento de antes(anterior) e o depois (prox), atualiza a lista
 l.imprimir()
 print("")
 
+# ->>>>>>Criar uma variável busca nó ou deixar assim?
 # Busca
-print(l.buscar(1))  # Saída: True
+print("O elemento 1 foi achado?", 
+      "Sim" if l.buscar(1) else "Não")  # Imprime o valor do vetor na posição 1 e Buscou o valor 1, Saída: True = Sim
 print("")
-print(l.buscar(9))  # Saída: False
+print("O elemento 9 foi achado?", 
+      "Sim" if l.buscar(9) else "Não")  # Imprime o valor do vetor na posição 9 e Buscou o valor 9, Saída: False = Não
+
+# Preciso colocar para ele imprimir apenas um valor, como faço isso?
--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # projeto1_ED1
 Projeto de implementação de várias estruturas de dados<br>
 Basear os Readmes dos projetos passados<br>
-Aprimorar esse Readme e deixar bem resumido do que eu fiz
+Aprimorar esse Readme e deixar bem resumido do que eu fiz<br><br>
 
-#####################revisar o que eu implementei no listas.zip############################################
+#####################Revisar o que eu implementei no listas.zip############################################<br><br>
+ <s> #####################Colocar sim ou nao na primeira lista?################################################# </s> <br><br>
+#####################Retirar os comentarios desnecessários nos codigos#####################################

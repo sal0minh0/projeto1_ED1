b120ec2cbf9cd11bce6a2bfe0e7745ab30b560bb
Salomão de Moraes <130030262+sal0minh0@users.noreply.github.com>
2024-10-12 16:02:21 +0000

    Adicionando o diretorio "Restaurante" para a pasta Python no cardapio

--- Restaurante/cardapio.py
+++ Restaurante/cardapio.py
@@ -1,3 +1,8 @@
+import sys
+import os
+
+# Adicionando o diretorio "Restaurante" para a pasta Python
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from Listas import lista_encadeada_simples
 
 class Cardapio:
@@ -77,4 +82,3 @@ print("")
 
 # Buscar item
 cardapio.buscar_item("Pizza")
-

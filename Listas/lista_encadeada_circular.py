c1d1ac4e9165e83c0f3701e2f986eb3659193c08
Salominho <salomaomoraes1@hotmail.com>
2024-10-16 11:30:37 -0300

    comentando essa lista

--- Listas/lista_encadeada_circular.py
+++ Listas/lista_encadeada_circular.py
@@ -25,64 +25,82 @@ class ListaEncadeadaCircular:
             self.cauda = novo_no
 
     def remover(self, valor):
-        """Remover o no da lista"""
+        """Remover o no da lista com um valor especificado"""
         if self.verificar_lista_vazia():
             print("A lista está vazia.") # Verificar se a lista esta vazia
             return
-        atual = self.cauda.proximo
-        anterior = self.cauda
+        
+        atual = self.cauda.proximo # Começa com o nó após a cauda, o primeiro nó
+        anterior = self.cauda # Acompanha o nó anterior
+        
         while True:
             if atual.valor == valor:
                 if atual == self.cauda and atual.proximo == self.cauda: # Se for o único no na lista
+                    # Se é o unico no na lista
                     self.cauda = None
                 else:
+                    # Remove o no atualizando o proximo ponteiro do no anterior
                     anterior.proximo = atual.proximo
-                    if atual == self.cauda: # Se for o no da cauda, movemos o no cauda
+                    if atual == self.cauda: 
+                        # Se remover o no cauda, atualizamos o no cauda
                         self.cauda = anterior
                 print(f"'{valor}' foi removido.")
                 return
+            
             anterior = atual
             atual = atual.proximo
+            
             if atual == self.cauda.proximo:
-                print(f"'{valor}' não foi encontrado.") # Caso não encontramos o valor
+                # Caso percorrermos toda a lista e não encontrarmos o valor
+                print(f"'{valor}' não foi encontrado.") 
                 break
 
     def buscar(self, valor):
-        """Busca valores na lista"""
+        """Busca valores na lista e retorna o no na posicao"""
         if self.verificar_lista_vazia():
             return None
+        
         posicao = 0
-        atual = self.cauda.proximo
+        atual = self.cauda.proximo # Começa com o nó após a cauda
+        
         while True:
             if atual.valor == valor:
                 return atual, posicao
+            
             atual = atual.proximo
             posicao += 1
+            
             if atual == self.cauda.proximo:
+                # Caso não encontre o valor na lista
                 return None, -1
 
     def atualizar(self, valor_atual, novo_valor):
-        """Atualizar um valor na lista"""
+        """Atualizar com um novo valor na lista"""
         no_encontrado, _ = self.buscar(valor_atual)
+        
         if no_encontrado:
             no_encontrado.valor = novo_valor
             return True
+        
         else:
             print(f"'{valor_atual}' não foi encontrado para atualizar.")
             print("")
             return False
         
-
     def imprimir(self):
         """Imprime a lista até o final"""
         if self.verificar_lista_vazia():
             print("A lista está vazia.")
             return
-        atual = self.cauda.proximo
+        
+        atual = self.cauda.proximo # Começa do nó após a cauda
+        
         while True:
-            print(atual.valor)  # Print each value on a new line
+            print(atual.valor)  # Imprime cada valor uma nova linha
             atual = atual.proximo
+            
             if atual == self.cauda.proximo:
+                # Se já percorrermos toda a lista
                 break
         
     def trocar_true_false_sim_nao(self, valor):

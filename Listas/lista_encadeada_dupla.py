c4084dbbde4a4b690c50b94d1ac6d32f587de709
Salominho <salomaomoraes1@hotmail.com>
2024-10-16 11:53:27 -0300

    comentando essa lista

--- Listas/lista_encadeada_dupla.py
+++ Listas/lista_encadeada_dupla.py
@@ -26,21 +26,25 @@ class ListaEncadeadaDupla:
             self.cauda = novo_no
 
     def remover(self, valor):
-        """Remover um nó da lista"""
+        """Remover um nó da lista com um valor especificado"""
         if self.verificar_lista_vazia():
             print("A lista está vazia, nada para remover.")
             return
         atual = self.cabeca
-        while atual: # Verificar percorrendo a lista, se o no é cauda ou cabeça e remover
+        while atual: 
+            # Percorrer a toda a lista para encontrar o nó com o valor fornecido
             if atual.valor == valor:
+                # Se o nó foi encontrado, atualize os ponteiros para remover o nó
                 if atual.anterior:
                     atual.anterior.prox = atual.prox
                 else:
+                    # Se é o cabeça da lista, atualize a cabeça 
                     self.cabeca = atual.prox
 
                 if atual.prox:
                     atual.prox.anterior = atual.anterior
                 else:
+                    # Se é o cauda da lista, atualize a cauda
                     self.cauda = atual.anterior
                 
                 print(f"'{valor}' foi removido da lista.")
@@ -49,20 +53,23 @@ class ListaEncadeadaDupla:
         print("Valor não encontrado na lista.")
 
     def buscar(self, valor):
-        """Busca valores na lista"""
+        """Busca valores na lista e retornar o nó na sua devida posicao"""
         atual = self.cabeca
         posicao = 0
         while atual:
-            if atual.valor == valor: # Se o valor atual for o que queremos retorne ele
+            if atual.valor == valor: 
+                # Se o valor atual for o que queremos, retorne ele e sua posição
                 return atual, posicao
             atual = atual.prox      
             posicao += 1
-        return None, -1 # Se não retorne falso
+            # Caso não encontre o nó
+        return None, -1 
 
     def atualizar(self, valor_atual, novo_valor):
         """Atualizar um valor na lista"""
         no_encontrado, _ = self.buscar(valor_atual)
         if no_encontrado:
+            # Se o no for encontrado, atualize o valor
             no_encontrado.valor = novo_valor
             print(f"'{valor_atual}' foi atualizado para '{novo_valor}'.")
             return True
@@ -77,10 +84,10 @@ class ListaEncadeadaDupla:
             return
         atual = self.cabeca
         while atual:
+            # Imprima cada valor, com uma nova linha no final da lista
             print(atual.valor, end = "\n" if atual.prox else "\n")
             atual = atual.prox
-            
-            
+                    
     def trocar_true_false_sim_nao(valor):
         """Trocar True or False por Sim e não"""
         return "Sim" if valor else "Não"

a6f8ee31a1df45c27f96981402c8f40efcfae11c
Salominho <salomaomoraes1@hotmail.com>
2024-10-16 11:45:29 -0300

    comentando essa lista

--- Listas/lista_encadeada_simples.py
+++ Listas/lista_encadeada_simples.py
@@ -59,7 +59,7 @@ class ListaEncadeadaSimples:
     def buscar(self, valor):
         """Busca um nó que contenha o valor que queremos"""
         atual = self.cabeca
-        posicao = 0 # Posicao do no
+        posicao = 0 # Iniciamos a posicao do no
         while atual is not None:  # Vai alcaçar até o final da lista
             if atual.valor == valor: # Verificar se o no atual tem um valor que procuramos
                 return atual, posicao
@@ -69,23 +69,23 @@ class ListaEncadeadaSimples:
     
     def atualizar_lista(self, anterior, novo_valor):
         """Atualiza o valor de um nó na lista"""
-        atual = self.cabeca
-        while atual is not None:
-            if atual.valor == anterior:
-                atual.valor = novo_valor
+        atual = self.cabeca # Começa no início, no no cabeca
+        while atual is not None: # Percorre a lista até o final
+            if atual.valor == anterior: # Se o valor atual é o que queremos atualizar
+                atual.valor = novo_valor # Atualiza o valor
                 return True
-            atual = atual.prox
-        return False
+            atual = atual.prox # Move para o próximo nó
+        return False # Caso não encontrar o valor
 
 
     def imprimir(self):
         """Exibe os elementos da lista encadeada"""
-        elementos = []
-        atual = self.cabeca
-        while atual:
-            elementos.append(atual.valor)
-            atual = atual.prox
-        print("\n".join(map(str, elementos)))
+        elementos = [] # Vetor para armazenar os elementos
+        atual = self.cabeca 
+        while atual: # Vamos até o final da lista
+            elementos.append(atual.valor) # Adiciona o valor atual ao vetor
+            atual = atual.prox 
+        print("\n".join(map(str, elementos))) # Imprime os elementos, um por linha
         
     def trocar_true_false_sim_nao(valor):
         """Trocar True or False por Sim e não"""
@@ -95,18 +95,18 @@ class ListaEncadeadaSimples:
     
     def somar(self):
         """Soma os valores da lista"""
-        atual = self.cabeca
-        soma = 0
-        while atual:
-            soma += atual.valor
-            atual = atual.prox
+        atual = self.cabeca 
+        soma = 0 # Inicializa a soma
+        while atual: # Percorre a lista até o final
+            soma += atual.valor # Adiciona o valor do nó atual à soma
+            atual = atual.prox 
         return soma
     
     def contar_elementos(self):
         """Conta o número de elementos na lista"""
-        atual = self.cabeca
-        contador = 0
+        atual = self.cabeca 
+        contador = 0 # Inicializamos
         while atual:
             contador += 1  # Incrementa o contador a cada nó encontrado
-            atual = atual.prox  # Move para o próximo nó
+            atual = atual.prox
         return contador
